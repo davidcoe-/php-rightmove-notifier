2df1f50d7028fd4c8a45d8ce45def0afa20e9582
David Coe <dc@covicstudio.com>
2014-02-24 21:36:30 +0000

    Started iterating and filtering results from Rightmove

--- callme.php
+++ callme.php
@@ -5,22 +5,47 @@ $scrapper = new rightmovescrap();
 $html = $scrapper->get_rightmove_listings();
 
 //var_dump($html);
-
+echo "Hello<br />";
 $img_links = array();
 $source_html = new DOMDocument();
 
 $source_html->loadHTML($html);
-$all_houses = $source_html->getElementsByTagName('li');
 
-print_r($all_houses);
 
-$all_houses = iterator_to_array($all_houses);
+//print_r($source_html);
+echo '<textarea rows="40">';
+$all_list_items = $source_html->getElementsByTagName('li');
 
-print_r($all_houses);
 
-foreach($all_houses->item as $house)
+foreach($all_list_items as $single_list_item)
 {
-	print_r($house);
+	//var_dump($single_list_item);
+	if ($single_list_item->getAttribute('name') === 'summary-list-item')
+	{
+		echo $single_list_item->getAttribute('name') ."<br />";
+	}
 }
 
+echo "<br /> ENDEND <br />";
+
+/*
+print_r($all_list_items);
+*/
+
+
+$temp_dom = new DOMDocument();
+foreach($all_list_items as $n)
+{
+	$temp_dom->appendChild($temp_dom->importNode($n,true));
+	var_dump($temp_dom);
+	echo "<br /> //////// <br />";
+
+}
+
+echo '</textarea>';
+
+
+//echo '<textarea rows="40">';
+print_r($temp_dom->saveHTML());
+//echo '</textarea>';
 ?>
