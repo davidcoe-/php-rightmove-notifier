ad3072f81365f981b617d63a95637aa5354d417e
David Coe <dc@covicstudio.com>
2014-02-27 20:41:34 +0000

    Getting key values out of each line item

--- callme.php
+++ callme.php
@@ -15,38 +15,43 @@ echo '<textarea rows="40">';
 $all_list_items = $source_html->getElementsByTagName('li');
 
 
+// For each list item found. Ensure it has a name element with the value 
+// of summary-list-item as these are the individual houses themselves.
 $temp_dom = new DOMDocument();
 foreach($all_list_items as $single_list_item)
 {
-	//var_dump($single_list_item);
+
 	if ($single_list_item->getAttribute('name') === 'summary-list-item')
 	{
-		echo $single_list_item->getAttribute('name') ."
-		";
+
+		// Get the house title out of the h2 tag.
+		$domnodelist_house_title = $single_list_item->getElementsByTagName('h2');
+		foreach ($domnodelist_house_title as $single_house_title) {
+			$house_title = $single_house_title->nodeValue;
+		}
+
+		// Get the house price out of the price class.
+		$domnodelist_price = $single_list_item->getElementsByTagName('p');
+		foreach ($domnodelist_price as $single_house_price)
+		{
+			if(stristr($single_house_price->nodeValue,'price'))
+			{
+				echo $single_house_price->nodeValue;
+			}
+		}
+
+		echo $house_price;
+
+		// Add the data to addition DOM for potential later use
 		$temp_dom->appendChild($temp_dom->importNode($single_list_item,true));
+
 	}
 }
 
-echo "
-END--END";
 
-/*
-print_r($all_list_items);
-*/
 
-/*
-$temp_dom = new DOMDocument();
-foreach($all_list_items as $n)
-{
-	$temp_dom->appendChild($temp_dom->importNode($n,true));
-	var_dump($temp_dom);
-	echo "<br /> //////// <br />";
-
-}
-*/
+echo "END--END";
 echo '</textarea>';
-
-
 
 //echo '<textarea rows="40">';
 print_r($temp_dom->saveHTML());
