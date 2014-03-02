affe91854b7659e5557f3cd506d5cc82e34d63d0
David Coe <dc@covicstudio.com>
2014-03-02 21:05:39 +0000

    Saved ID's to a file for later reading and comparing too

--- callme.php
+++ callme.php
@@ -8,74 +8,63 @@
 <?php
 
 $rightmove_url = 'http://www.rightmove.co.uk/property-for-sale/find.html?searchType=SALE&locationIdentifier=REGION%5E347&insId=2&radius=0.0&displayPropertyType=&minBedrooms=&maxBedrooms=&minPrice=&maxPrice=&retirement=&partBuyPartRent=&maxDaysSinceAdded=&_includeSSTC=on&sortByPriceDescending=&primaryDisplayPropertyType=&secondaryDisplayPropertyType=&oldDisplayPropertyType=&oldPrimaryDisplayPropertyType=&newHome=&auction=false';
-$file_location = $_SERVER['DOCUMENT_ROOT'] .'/houses_found.json';
+$file_location = $_SERVER['DOCUMENT_ROOT'] .'/houses_found.csv';
 
 require_once('rightmovescrap.php');
 $houses_raw_html = new rightmovescrap();
 $all_houses = $houses_raw_html->get_rightmove_listings($rightmove_url);
 $all_houses_json = json_encode($all_houses);
 
-echo "<pre>";
-var_dump($all_houses);
-echo "</pre>";
-
-
 /** **********************************************************************************
 	Now to start using files to record and compare against previous found items
 ********************************************************************************** **/
 
-
-// Check if file exists
+// Check if file with id's exists
 if (file_exists($file_location))
 {
+	// Get the id from the file and convert to a PHP array
 	$existing_houses = file_get_contents($file_location);
-	$existing_houses = json_decode($existing_houses, true);
+	$existing_houses = str_getcsv($existing_houses);
 
-	foreach ($all_houses as $value)
+	// Get each new house
+	foreach ($all_houses as $new_house)
 	{
-		echo "<pre>";
-		var_dump($value['id']);
-		echo "</pre>";	
+		$existing_houses_count = count($existing_houses) - 1;
+		foreach ($existing_houses as $existing_house)
+		{
+			if (!in_array($new_house['id'], $existing_house))
+			{
+				echo "\n ". key($existing_house) ." Found! \n";
+				// add to array for emailing later
+			}
 
-		if (in_array($value['id'], $existing_houses))
-			{echo "\n Found! \n";}
-			else {echo "\n NOT Found! \n";}
+			echo "\n Blah! \n";
+			// add to array for saving later
+		}
+
 	}
 
 } else {
 
 }
 
-echo "\n\n Existing Houses Array \n\n";
-echo "<pre>";
-var_dump($existing_houses);
-echo "</pre>";
-
-/**
-$file = @fopen($file_location,"x");
-if($file)
-{
-	echo fwrite($file,"Some Code Here"); 
-	fclose($file); 
-}
-*/
-
-
-
-
-
 
 /*
+// write to a file
+foreach ($all_houses as $single_house) {
+	$ids_to_record = $ids_to_record.$single_house['id'] .',';
+}
+
+var_dump($ids_to_record);
+
+
+
 $f = (file_exists($file_location))? fopen($file_location, "a+") : fopen($file_location, "w+");
 
-fwrite($f, $all_houses_json);
+fwrite($f, $ids_to_record);
 fclose($f);
 chmod($file_location, 0777);
 */
-
-
-	//$all_houses = json_decode($all_houses, true);
-
 
 ?>
 
